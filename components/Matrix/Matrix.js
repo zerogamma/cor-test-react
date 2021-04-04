806dc333d6bd1a0c6e7328a65d099ef36b0a57d9
zerogamma <zeroalpha2000@gmail.com>
2021-04-03 21:05:36 -0300

    2 files modified

--- components/Matrix/Matrix.js
+++ components/Matrix/Matrix.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { MContainer, MBox } from "./style";
 
 const Matrix = () => {
   const matrix = useSelector(state => state.matrix.selectedMatrix);
   console.log("matrix!");
   console.log(matrix);
-  return <div />;
+  return (
+    <MContainer>
+      {
+        matrix.map(row => {
+          row.map(column => {
+            return <MBox>{column}</MBox>
+          })
+        })
+      }
+    </MContainer>
+  );
 };
 
 export default Matrix;
--- components/Matrix/style.js
+++ components/Matrix/style.js
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const MContainer = styled.div``;
+
+export const MBox = styled.div`
+  border: 1px solid;
+  width: 25px;
+  height: 25px;
+  border-radius: 5px;
+`;
