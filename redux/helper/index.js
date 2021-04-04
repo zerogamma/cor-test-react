3deea924508917af87a85e60a58e151b2c1807a6
zerogamma <zeroalpha2000@gmail.com>
2021-04-04 16:08:20 -0300

    1 file modified

--- redux/helper/index.js
+++ redux/helper/index.js
@@ -1,53 +1,52 @@
 export const checkSide = (matrix, x, y, direction, word) => {
   try {
-    switch (direction) {
-      case "up":
-        if (matrix[y - 1][x] === word[1] && matrix[y - 2][x] === word[2])
-          return true;
-        return false;
-      case "upright":
-        if (
-          matrix[y - 1][x + 1] === word[1] &&
+    const validation = {
+      up: function(matrix, x, y, word) {
+        return matrix[y - 1][x] === word[1] && matrix[y - 2][x] === word[2]
+          ? true
+          : false;
+      },
+      upright: function(matrix, x, y, word) {
+        return matrix[y - 1][x + 1] === word[1] &&
           matrix[y - 2][x + 2] === word[2]
-        )
-          return true;
-        return false;
-      case "right":
-        if (matrix[y][x + 1] === word[1] && matrix[y][x + 2] === word[2])
-          return true;
-        return false;
-      case "rightdown":
-        if (
-          matrix[y + 1][x + 1] === word[1] &&
+          ? true
+          : false;
+      },
+      right: function(matrix, x, y, word) {
+        return matrix[y][x + 1] === word[1] && matrix[y][x + 2] === word[2]
+          ? true
+          : false;
+      },
+      rightdown: function(matrix, x, y, word) {
+        return matrix[y + 1][x + 1] === word[1] &&
           matrix[y + 2][x + 2] === word[2]
-        )
-          return true;
-        return false;
-      case "down":
-        if (matrix[y + 1][x] === word[1] && matrix[y + 2][x] === word[2])
-          return true;
-        return false;
-      case "downleft":
-        if (
-          matrix[y + 1][x - 1] === word[1] &&
+          ? true
+          : false;
+      },
+      down: function(matrix, x, y, word) {
+        return matrix[y + 1][x] === word[1] && matrix[y + 2][x] === word[2]
+          ? true
+          : false;
+      },
+      downleft: function(matrix, x, y, word) {
+        return matrix[y + 1][x - 1] === word[1] &&
           matrix[y + 2][x - 2] === word[2]
-        )
-          return true;
-        return false;
-      case "left":
-        if (matrix[y][x - 1] === word[1] && matrix[y][x - 2] === word[2])
-          return true;
-        return false;
-      case "leftup":
-        if (
-          matrix[y - 1][x - 1] === word[1] &&
+          ? true
+          : false;
+      },
+      left: function(matrix, x, y, word) {
+        return matrix[y][x - 1] === word[1] && matrix[y][x - 2] === word[2]
+          ? true
+          : false;
+      },
+      leftup: function(matrix, x, y, word) {
+        return matrix[y - 1][x - 1] === word[1] &&
           matrix[y - 2][x - 2] === word[2]
-        )
-          return true;
-        return false;
-      default:
-        return false;
-    }
+          ? true
+          : false;
+      }
+    };
+    return validation[direction](matrix, x, y, word);
   } catch (e) {
     return false;
   }
