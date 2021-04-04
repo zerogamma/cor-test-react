06041f2dd715944c9ddac6b2badc7b1994cd2f2e
zerogamma <zeroalpha2000@gmail.com>
2021-04-04 14:51:27 -0300

    1 file modified

--- containers/AssignmentContainer.js
+++ containers/AssignmentContainer.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Heading from "../components/Heading";
 import { ACMain, AHButton, AContainer } from "./AssignmentContainerStyle";
 
 const AssignmentContainer = () => {
-  const [visibility, setvisibility] = useState(false);
+  const [visibility, setvisibility] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setvisibility(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [visibility]);
 
   return (
     <ACMain>
