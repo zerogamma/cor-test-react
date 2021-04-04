5aae46ed5315a8dff86d2553aa5b778b1cc19c22
zerogamma <zeroalpha2000@gmail.com>
2021-04-04 15:31:38 -0300

    1 file modified

--- components/Result/Result.js
+++ components/Result/Result.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { RContainer, RLabel } from "./style";
+import { RContainer, RLabel, RResult } from "./style";
 
 const Result = () => {
   const toFind = useSelector(state => state.finder.finded_word);
   return (
     <RContainer>
       <RLabel>
-        There are {toFind} "OIE" word combination in this Alphabet Soup try to
-        find them!
+        There are <RResult>{toFind}</RResult> "O I E" word combination in this
+        Alphabet Soup try to find them!
       </RLabel>
     </RContainer>
   );
