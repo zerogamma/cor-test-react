3ce5ebd21a657d6eb290fefb6692b396cef7f5a5
zerogamma <zeroalpha2000@gmail.com>
2021-04-04 14:33:01 -0300

    2 files modified

--- components/Header/style.js
+++ components/Header/style.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const HContainer = styled.div`
   font-family: "Londrina Solid", cursive;
+  border-bottom: 1px solid #dadce0;
+  display:flex;
+  
 `;
 
 export const HTitle = styled.div``;
