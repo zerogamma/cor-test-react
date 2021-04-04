8da9336a4dfbd66838fff31d3d5445ace4f9c946
zerogamma <zeroalpha2000@gmail.com>
2021-04-04 13:34:36 -0300

    3 files modified

---
+++ components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {HContainer, HTitle} from "./style"
+
+
+const Header = () => {
+  return <HContainer><HTitle>Alphabet Soup</HTitle></HContainer>
+}
+
+export const Header
---
+++ components/Header/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const HContainer = styled.div``;
+
+export const HTitle = styled.div``;
