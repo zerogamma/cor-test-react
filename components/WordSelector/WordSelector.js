70d2647d168c50da06113f711e127942c2de86e4
zerogamma <zeroalpha2000@gmail.com>
2021-04-05 00:28:14 -0300

    1 file modified

--- components/WordSelector/WordSelector.js
+++ components/WordSelector/WordSelector.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import _ from "lodash";
-import { WSContainer, WSLabel, WSInput } from "./style";
+import {
+  WSContainer,
+  WSLabel,
+  WSInput,
+  WSConfirm,
+  WSShowContainer,
+  WSShowButton,
+  WSShowSpan
+} from "./style";
 
 const WordSelector = () => {
   const currentWord = useSelector(state => state.finder.word);
   const [word, setWord] = useState(currentWord.join(""));
+  const dispatch = useDispatch();
+  const inputRef = useRef(null);
+
+  const newWord = element => {
+    setWord([...inputRef.current.value]);
+
+  };
 
   return (
     <WSContainer>
       <WSLabel>
-        Current Word to Find is{" "}
-        <WSInput value={word} onChange={e => setWord(e.target.value)} />
+        Current Word to Find is <WSInput defaultValue={word} ref={inputRef} />
+        <WSConfirm type="button" value="Change" onClick={newWord} />
       </WSLabel>
     </WSContainer>
   );
--- components/WordSelector/style.js
+++ components/WordSelector/style.js
@@ -5,3 +5,11 @@ export const WSContainer = styled.div``;
 export const WSLabel = styled.div``;
 
 export const WSInput = styled.input``;
+
+export const WSConfirm = styled.input``;
+
+export const WSShowContainer = styled.label``;
+
+export const WSShowButton = styled.input``;
+
+export const WSShowSpan = styled.span``;
